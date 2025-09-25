1a9fadaf41459b12c2c3c9aec05c80815da6e60a
paulf81 <paul.fleming@nrel.gov>
2025-09-25 11:30:38 -0600

    Remove un-needed par blocking

--- examples_artificial_data/05_model_fit/02a_optimize_parameter_optsweep.py
+++ examples_artificial_data/05_model_fit/02a_optimize_parameter_optsweep.py
@@ -11,55 +11,54 @@ from flasc.model_fitting.opt_library import opt_sweep
 In this example using the opt_sweep optimization routine from the opt_library.
 """
 
-# Since ModelFit is always parallel this is important to include
-if __name__ == "__main__":
-    n_grid = 10
 
-    # Load the data from previous example
-    with open("two_turbine_data.pkl", "rb") as f:
-        data = pickle.load(f)
+n_grid = 10
 
-    # Unpack
-    df = data["df"]
-    fm_default = data["fm_default"]
-    parameter = data["parameter"]
-    we_value_original = data["we_value_original"]
-    we_value_set = data["we_value_set"]
+# Load the data from previous example
+with open("two_turbine_data.pkl", "rb") as f:
+    data = pickle.load(f)
 
-    # Now pass the above cost function to the ModelFit class
-    mf = ModelFit(
-        df,
-        fm_default,
-        TurbinePowerMeanAbsoluteError(),
-        parameter_list=[parameter],
-        parameter_name_list=["wake expansion"],
-        parameter_range_list=[(0.01, 0.07)],
-        parameter_index_list=[],
-    )
+# Unpack
+df = data["df"]
+fm_default = data["fm_default"]
+parameter = data["parameter"]
+we_value_original = data["we_value_original"]
+we_value_set = data["we_value_set"]
 
-    # Compute the baseline cost
-    print("Evaluating baseline cost")
-    baseline_cost = mf.evaluate_floris()
+# Now pass the above cost function to the ModelFit class
+mf = ModelFit(
+    df,
+    fm_default,
+    TurbinePowerMeanAbsoluteError(),
+    parameter_list=[parameter],
+    parameter_name_list=["wake expansion"],
+    parameter_range_list=[(0.01, 0.07)],
+    parameter_index_list=[],
+)
 
-    # Optimize
-    opt_result = opt_sweep(mf, n_grid=n_grid)
+# Compute the baseline cost
+print("Evaluating baseline cost")
+baseline_cost = mf.evaluate_floris()
 
-    print("Final optimization results (opt_result dictionary):")
-    print(opt_result)
+# Optimize
+opt_result = opt_sweep(mf, n_grid=n_grid)
 
-    # Print results
-    print("----------------------------")
-    print(f"Default parameter: {we_value_original}")
-    print(f"Set parameter: {we_value_set}")
-    print()
-    print(f"Calibrated parameter value:  {opt_result['optimized_parameter_values'][0]:.2f}")
-    print("----------------------------")
+print("Final optimization results (opt_result dictionary):")
+print(opt_result)
 
-    # Plot the results
-    plt.plot(opt_result["all_parameter_combinations"], opt_result["all_costs"])
-    plt.axvline(we_value_original, color="k", linestyle="--", label="Original value")
-    plt.axvline(we_value_set, color="r", linestyle="--", label="Set value")
-    plt.xlabel("Wake expansion value")
-    plt.ylabel("Cost value")
-    plt.legend()
-    plt.show()
+# Print results
+print("----------------------------")
+print(f"Default parameter: {we_value_original}")
+print(f"Set parameter: {we_value_set}")
+print()
+print(f"Calibrated parameter value:  {opt_result['optimized_parameter_values'][0]:.2f}")
+print("----------------------------")
+
+# Plot the results
+plt.plot(opt_result["all_parameter_combinations"], opt_result["all_costs"])
+plt.axvline(we_value_original, color="k", linestyle="--", label="Original value")
+plt.axvline(we_value_set, color="r", linestyle="--", label="Set value")
+plt.xlabel("Wake expansion value")
+plt.ylabel("Cost value")
+plt.legend()
+plt.show()
--- examples_artificial_data/05_model_fit/02b_optimize_parameter_optuna.py
+++ examples_artificial_data/05_model_fit/02b_optimize_parameter_optuna.py
@@ -17,55 +17,53 @@ of optuna's provided visualization functions are used to assess the study: plot_
 and plot_slice.
 """
 
-# Since ModelFit is always parallel this is important to include
-if __name__ == "__main__":
-    # Parameters
-    time_out = 5
+# Parameters
+time_out = 5
 
-    # Load the data from previous example
-    with open("two_turbine_data.pkl", "rb") as f:
-        data = pickle.load(f)
+# Load the data from previous example
+with open("two_turbine_data.pkl", "rb") as f:
+    data = pickle.load(f)
 
-    # Unpack
-    df = data["df"]
-    fm_default = data["fm_default"]
-    parameter = data["parameter"]
-    we_value_original = data["we_value_original"]
-    we_value_set = data["we_value_set"]
+# Unpack
+df = data["df"]
+fm_default = data["fm_default"]
+parameter = data["parameter"]
+we_value_original = data["we_value_original"]
+we_value_set = data["we_value_set"]
 
-    # Now pass the above cost function to the ModelFit class
-    mf = ModelFit(
-        df,
-        fm_default,
-        TurbinePowerMeanAbsoluteError(),
-        parameter_list=[parameter],
-        parameter_name_list=["wake expansion"],
-        parameter_range_list=[(0.01, 0.07)],
-        parameter_index_list=[],
-    )
+# Now pass the above cost function to the ModelFit class
+mf = ModelFit(
+    df,
+    fm_default,
+    TurbinePowerMeanAbsoluteError(),
+    parameter_list=[parameter],
+    parameter_name_list=["wake expansion"],
+    parameter_range_list=[(0.01, 0.07)],
+    parameter_index_list=[],
+)
 
-    # Compute the baseline cost
-    print("Evaluating baseline cost")
-    baseline_cost = mf.evaluate_floris()
+# Compute the baseline cost
+print("Evaluating baseline cost")
+baseline_cost = mf.evaluate_floris()
 
-    # Optimize
-    opt_result = opt_optuna(mf, timeout=time_out, n_trials=None)
+# Optimize
+opt_result = opt_optuna(mf, timeout=time_out, n_trials=None)
 
-    # Print results
-    print("----------------------------")
-    print(f"Default parameter: {we_value_original}")
-    print(f"Set parameter: {we_value_set}")
-    print()
-    print(f"Calibrated parameter value:  {opt_result['optimized_parameter_values'][0]:.2f}")
-    print("----------------------------")
+# Print results
+print("----------------------------")
+print(f"Default parameter: {we_value_original}")
+print(f"Set parameter: {we_value_set}")
+print()
+print(f"Calibrated parameter value:  {opt_result['optimized_parameter_values'][0]:.2f}")
+print("----------------------------")
 
-    # Show an optuna progress plot
-    plot_optimization_history(opt_result["optuna_study"])
+# Show an optuna progress plot
+plot_optimization_history(opt_result["optuna_study"])
 
-    # Show a slice plot
-    ax = plot_slice(opt_result["optuna_study"])
-    ax.axvline(we_value_original, color="k", linestyle="--", label="Original value")
-    ax.axvline(we_value_set, color="r", linestyle="--", label="Set value")
-    ax.legend()
+# Show a slice plot
+ax = plot_slice(opt_result["optuna_study"])
+ax.axvline(we_value_original, color="k", linestyle="--", label="Original value")
+ax.axvline(we_value_set, color="r", linestyle="--", label="Set value")
+ax.legend()
 
-    plt.show()
+plt.show()
