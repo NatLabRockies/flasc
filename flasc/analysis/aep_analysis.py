132aba76fbfcc878a26b2b13bdada2cb4d9f5fe0
Bart Doekemeijer <bart.doekemeijer@shell.com>
2026-03-03 10:37:16 +0100

    Rename from AEP to cumulative production. Add warnings when numbers are not a whole multiple of 12 months.

--- flasc/analysis/aep_analysis.py
+++ flasc/analysis/aep_analysis.py
@@ -24,17 +24,22 @@ def _print_pretty_table(table_dict, title):
     return print(mrkdwn)
 
 
-def compare_absolute_aep_and_relative_wake_loss(
+def compare_cumulative_production_and_relative_wake_loss(
     df_list,  # Typical use case: 1st entry is SCADA, then remaining are models to compare. All models are compared against 1st entry.
     df_upstream,
     exclude_turbs=[],
     model_tags=None,
     print_to_console=True,
 ):
-    # Input formatting options
-    if isinstance(df_list, pd.DataFrame):
-        df_list = [df_list]
+    # Check input dataframes are consistent in terms of number of turbines and timestamps
+    if not all([all(df_list[0]["time"] == df["time"]) for df in df_list]):
+        raise ValueError("Input dataframes have different timestamps. Please ensure all dataframes have the same timestamps.")
 
+    n_turbs_list = [dfm.get_num_turbines(df) for df in df_list]
+    if len(set(n_turbs_list)) > 1:
+        raise ValueError(f"Input dataframes have different number of turbines: {n_turbs_list}. Please ensure all dataframes have the same number of turbines.")
+
+    # Apply default model tags if not provided
     if model_tags is None:
         model_tags = [f"Model {ti}" for ti in range(len(df_list))]
 
@@ -71,11 +76,27 @@ def compare_absolute_aep_and_relative_wake_loss(
         df = dfm.set_pow_ref_by_upstream_turbines(df, df_upstream, exclude_turbs=exclude_turbs)
 
     #######################################################################################################################
-    ################## Compare absolute AEP directly between SCADA and simulated data (LES) ###############################
+    ################## Compare cumulative production directly between SCADA and simulated data (LES) #####################
     #######################################################################################################################
 
-    # Absolute AEP for the entire farm and per turbine
-    aep_turbine_list = [[df[pc].sum() for pc in pow_cols] for df in df_list]
+    # Check if our dataset is a whole multiple of 8760 hours (1 year), if not, raise a warning that the AEP calculation is not
+    # exact and that we are comparing cumulative energy rather than AEP. This is just a sanity check, not a strict requirement.
+    t0 = df_list[0].iloc[0]["time"]
+    t1 = df_list[0].iloc[-1]["time"]
+    timespan = (t1 - t0)
+    n_measurements_per_hour = np.timedelta64(1, 'h') / (df_list[0].iloc[1]["time"] - df_list[0].iloc[0]["time"])
+    n_hours_total = timespan / np.timedelta64(1, 'h')
+    n_weeks = n_hours_total / 168
+    n_hours_in_year = 8760
+    avg_no_years_in_data = round(n_hours_total / n_hours_in_year)
+    avg_no_years_in_data = np.max([avg_no_years_in_data, 1])  # Minimum of 1 year needed to have cumulative production be comparable to AEP
+    offset_prct = 100.0 * (n_hours_total - avg_no_years_in_data * n_hours_in_year) / n_hours_in_year # Offset percentage from whole number of years
+    if np.abs(offset_prct) > 1.0:
+        print(f"WARNING: Dataset spans {n_weeks:.1f} weeks and is not a whole multiple of 8760 hours (1 year). Note the " +
+        f"reported numbers are CUMULATIVE ENERGY, not AEP. Offset from whole number of years: {offset_prct:+.2f}%.")
+
+    # Absolute cumulative energy production for the entire farm and per turbine
+    aep_turbine_list = [[(df[pc].sum() / n_measurements_per_hour) for pc in pow_cols] for df in df_list]
     aep_farm_list = [np.sum(aep_tm) for aep_tm in aep_turbine_list]
 
     table_absolute_aep_dict = {
@@ -90,11 +111,11 @@ def compare_absolute_aep_and_relative_wake_loss(
             )
 
     #######################################################################################################################
-    ################ Now we do the same exercise, but with wake loss rather than absolute AEP #############################
+    ################ Now we do the same exercise, but with wake loss rather than cumulative production ####################
     #######################################################################################################################
 
 
-    # Compare AEP wake loss relative to most upstream turbines
+    # Compare cumulative energy wake loss relative to most upstream turbines
     aep_turbine_waked_list = [np.array([None for _ in range(n_turbs)], dtype=float) for _ in df_list]
     aep_turbine_unwaked_list = [np.array([None for _ in range(n_turbs)], dtype=float) for _ in df_list]
 
@@ -104,8 +125,8 @@ def compare_absolute_aep_and_relative_wake_loss(
             p_test = np.array(df[f"pow_{ti:03d}"], dtype=float, copy=True)
             p_ref = np.array(df["pow_ref"], dtype=float, copy=True)
             ids_non_nan = (~np.isnan(p_test)) & (~np.isnan(p_ref))
-            aep_turbine_unwaked_list[dii][ti] = np.sum(p_ref[ids_non_nan])
-            aep_turbine_waked_list[dii][ti] = np.sum(p_test[ids_non_nan])
+            aep_turbine_unwaked_list[dii][ti] = np.sum(p_ref[ids_non_nan]) / n_measurements_per_hour
+            aep_turbine_waked_list[dii][ti] = np.sum(p_test[ids_non_nan]) / n_measurements_per_hour
 
     aep_farm_waked_list = [np.sum(aep_tm_waked) for aep_tm_waked in aep_turbine_waked_list]
     aep_farm_unwaked_list = [np.sum(aep_tm_unwaked) for aep_tm_unwaked in aep_turbine_unwaked_list]
